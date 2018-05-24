92bef1b68642d1721e7314e634b41bb3b57af9bf
samrullo <amrulloev.subhon@gmail.com>
2018-05-24 20:27:59 +0900

    update 20180524 20:27

--- app/routers/user.server.router.js
+++ app/routers/user.server.router.js
@@ -4,6 +4,7 @@ module.exports=function(app){
   app.route('/users').post(users.create).get(users.list);
   app.route('/users/:userId').get(users.read).put(users.update).delete(users.delete);
   app.route('/users/:username').get(users.read).put(users.update);
+  app.route('/pagecount').get(users.list);
   app.param('userId',users.userById);
   app.param('username',users.userByUsername);
 
--- server.js
+++ server.js
@@ -6,41 +6,6 @@ var db=mongoose();
 var app=express();
 var port=process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip=process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
-app.get('/', function (req, res) {
-  // try to initialize the db on every request if it's not already
-  // initialized.
-  if (!db) {
-    initDb(function(err){});
-  }
-  if (db) {
-    var col = db.collection('counts');
-    // Create a document with request IP and current time of request
-    col.insert({ip: req.ip, date: Date.now()});
-    col.count(function(err, count){
-      if (err) {
-        console.log('Error running count. Message:\n'+err);
-      }
-      res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
-    });
-  } else {
-    res.render('index.html', { pageCountMessage : null});
-  }
-});
-
-app.get('/pagecount', function (req, res) {
-  // try to initialize the db on every request if it's not already
-  // initialized.
-  if (!db) {
-    initDb(function(err){});
-  }
-  if (db) {
-    db.collection('counts').count(function(err, count ){
-      res.send('{ pageCount: ' + count + '}');
-    });
-  } else {
-    res.send('{ pageCount: -1 }');
-  }
-});
 
 
 app.listen(port,ip);
